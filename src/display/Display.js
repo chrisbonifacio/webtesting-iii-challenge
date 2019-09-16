0bebf77f02a8bfeb7e49c385faf399b8c9d8e3a3
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-16 18:11:37 -0400

    add data-testid to elements
    Co-authored-by Moses Samuel <mosamuel98@gmail.com>

--- src/display/Display.js
+++ src/display/Display.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React from "react"
 
 const Display = ({ closed, locked }) => {
-  const closedClass = `led ${closed ? 'red-led' : 'green-led'}`;
-  const lockedClass = `led ${locked ? 'red-led' : 'green-led'}`;
+  const closedClass = `led ${closed ? "red-led" : "green-led"}`
+  const lockedClass = `led ${locked ? "red-led" : "green-led"}`
 
   return (
-    <div className="display panel">
-      <div className={lockedClass}>{locked ? 'Locked' : 'Unlocked'}</div>
-      <div className={closedClass}>{closed ? 'Closed' : 'Open'}</div>
+    <div data-testid="display" className="display panel">
+      <div data-testid="lock-status" className={lockedClass}>
+        {locked ? "Locked" : "Unlocked"}
+      </div>
+      <div data-testid="open-status" className={closedClass}>
+        {closed ? "Closed" : "Open"}
+      </div>
     </div>
-  );
-};
+  )
+}
 
 Display.defaultProps = {
   closed: false,
-  locked: false,
-};
+  locked: false
+}
 
-export default Display;
+export default Display
