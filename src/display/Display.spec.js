f3a22e30ecb98837e95a165380a0cdaf5698e237
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-16 18:11:57 -0400

    write test for Display
    Co-authored-by Moses Samuel <mosamuel98@gmail.com>

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,29 @@
+import React from "react"
+import { render, fireEvent } from "@testing-library/react"
+
+import Dashboard from "../dashboard/Dashboard"
+
 // Test away!
+
+test("Display updates correctly when lock is toggled", () => {
+  const { getByText, queryByTestId, getByTestId } = render(<Dashboard />)
+
+  // Arrange
+  // gather elements to be tested
+  const display = queryByTestId("display")
+  const openCloseButton = getByTestId("open-close-button")
+  const lockButton = getByTestId("lock-button")
+  const lockStatus = queryByTestId("lock-status")
+  const openStatus = queryByTestId("open-status")
+
+  // Act`
+  // fire off events
+  fireEvent.click(openCloseButton)
+
+  // Assert
+  // evaluate changes
+  expect(Array.from(display.classList)).toEqual(["display", "panel"])
+  expect(Array.from(openStatus.classList)).toEqual(["led", "red-led"])
+  expect(openStatus.textContent).toMatch("Closed")
+  expect(openCloseButton.textContent).toMatch("Open Gate")
+})
